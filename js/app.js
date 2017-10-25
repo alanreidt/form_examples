46c961e67a8af34851e45351d0cee8ee00f7f44c
Alan Reid <alanreidt@gmail.com>
2017-10-25 15:38:53 +0300

    Edit the progress-bar html,css,js. Now it works, but stays a dirty.

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ var forms = document.getElementsByClassName('form');
 var forms_len = forms.length;
 
 var confirmReset = function(evt) {
-	if (!confirm('Are you sure?')) {
+	if (!confirm('Do you really want to reset this data? If no, select Cancel below.')) {
 		evt.preventDefault();
 	}
 };
@@ -38,14 +38,45 @@ var confirmReset = function(evt) {
 for (var i = 0; i < forms_len; i++) {
 	var form = forms[i];
 
-	addEventListener('reset', confirmReset);
+	form.addEventListener('reset', confirmReset);
 }
 
-// progress-bar (don`t work)
+// Progress-bar toggle
 
-var shipping = document.getElementById('shipping');
+// TODO:
+//   clean up functions;
+//   edit a progress-bar__items' id;
+//   edit progress-bar styles;
 
-shipping.onclick = function() {
-	alert("alarm");
-	this.classList.toggle('progress-bar__link_active', true);
+var progressBarItems = document.getElementsByClassName('progress-bar__item');
+var progressBarItems_len = progressBarItems.length;
+
+var addProgressBarActiveClass = function() {
+	var items = document.getElementsByClassName('progress-bar__item');
+	var items_len = items.length;
+
+	var progressBar = document.getElementById('checkout__progress-bar');
+	var progressBarClass = 'progress-bar_active_' + this.id;
+
+	for (var i = 0; i < items_len; i++) {
+		var item = items[i];
+
+		removeClass('progress-bar__item_active', item);
+	}
+
+	this.classList.toggle('progress-bar__item_active', true);
+
+	progressBar.classList.toggle(progressBar.classList[1], false);
+	progressBar.classList.toggle(progressBarClass, true);
 };
+
+var removeClass = function(cls, elem) {
+	elem.classList.toggle(cls, false);
+};
+
+for (var i = 0; i < progressBarItems_len; i++) {
+	var progressBarItem = progressBarItems[i];
+
+	progressBarItem.addEventListener('click', addProgressBarActiveClass);
+	progressBarItem.addEventListener('keypress', addProgressBarActiveClass);
+}
